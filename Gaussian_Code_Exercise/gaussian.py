06aeccc8d9c012257e48ec2ab2f39417cf879878
Combatd <combatdstar@gmail.com>
2020-06-02 23:02:04 -0700

    Define Gaussian#pdf

--- Gaussian_Code_Exercise/gaussian.py
+++ Gaussian_Code_Exercise/gaussian.py
@@ -148,7 +148,7 @@ class Gaussian():
         
         # TODO: Calculate the probability density function of the Gaussian distribution
         #       at the value x. You'll need to use self.stdev and self.mean to do the calculation
-        pass        
+        return (1.0 / (self.stdev * math.sqrt(2*math.pi))) * math.exp(-0.5*((x - self.mean) / self.stdev) ** 2)
 
     def plot_histogram_pdf(self, n_spaces = 50):
 
